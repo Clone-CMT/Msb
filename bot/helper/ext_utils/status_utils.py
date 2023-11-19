62b3c00186accb72bec9de1e0acd67465fd9b5de
Clone-CMT <149989739+Clone-CMT@users.noreply.github.com>
2023-11-19 12:21:30 +0700

    fix

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -134,7 +134,7 @@ def get_readable_message(sid, is_user, page_no=1, status="All", page_step=1):
         tasks[start_position : STATUS_LIMIT + start_position], start=1
     ):
         tstatus = task.status()
-        msg += f"💾 Nama: <blockquote><code>{escape(f'{task.name()}')}</code></blockquote>"
+        msg += f"💾 Nama:<blockquote><code>{escape(f'{task.name()}')}</code></blockquote>"
         msg += f"\n┌┤{get_progress_bar_string(task.progress())}[<code>{task.progress()}</code>]"
         if task.listener.isSuperChat:
             msg += f"\n<b>├📲 Status :</b> <a href='{task.listener.message.link}'>{tstatus}</a>"
